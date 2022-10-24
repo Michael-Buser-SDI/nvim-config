0d30a443b38f2b7b090c20ccf48ad8a609e0c0ef
Michael Buser <michael.buser@searchdiscovery.com>
2022-10-24 13:38:37 -0400

    init script

---
+++ init.sh
@@ -0,0 +1,8 @@
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+./nvim.appimage --appimage-extract
+sudo mv squashfs-root /
+sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
+rm nvim.appimage
+brew install ripgrep
+git clone https://github.com/AstroNvim/AstroNvim ~/.config/nvim\nnvim +PackerSync
